a754c0722e6e265c91e52c230e8171986ad97d4c
Ryan Sang <ryan.sang@seeq.com>
2021-09-30 09:24:05 -0500

    CRAB-26905: Removed formatting changes.

--- src/main/java/javax/measure/converter/UnitConverter.java
+++ src/main/java/javax/measure/converter/UnitConverter.java
@@ -14,14 +14,14 @@ import java.io.Serializable;
  * <p> This class represents a converter of numeric values.</p>
  *
  * <p> It is not required for sub-classes to be immutable
- * (e.g. currency converter).</p>
+ *     (e.g. currency converter).</p>
  *
  * <p> Sub-classes must ensure unicity of the {@link #IDENTITY identity}
- * converter. In other words, if the result of an operation is equivalent
- * to the identity converter, then the unique {@link #IDENTITY} instance
- * should be returned.</p>
+ *     converter. In other words, if the result of an operation is equivalent
+ *     to the identity converter, then the unique {@link #IDENTITY} instance
+ *     should be returned.</p>
  *
- * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @author  <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @version 3.1, April 22, 2006
  */
 public abstract class UnitConverter implements Serializable {
@@ -50,11 +50,9 @@ public abstract class UnitConverter implements Serializable {
     /**
      * Converts a double value.
      *
-     * @param x
-     *         the numeric value to convert.
+     * @param  x the numeric value to convert.
      * @return the converted numeric value.
-     * @throws ConversionException
-     *         if an error occurs during conversion.
+     * @throws ConversionException if an error occurs during conversion.
      */
     public abstract double convert(double x) throws ConversionException;
 
@@ -63,8 +61,8 @@ public abstract class UnitConverter implements Serializable {
      * <code>convert(u + v) == convert(u) + convert(v)</code> and
      * <code>convert(r * u) == r * convert(u)</code>.
      * For linear converters the following property always hold:[code]
-     * y1 = c1.convert(x1);
-     * y2 = c2.convert(x2);
+     *     y1 = c1.convert(x1);
+     *     y2 = c2.convert(x2);
      * then y1*y2 = c1.concatenate(c2).convert(x1*x2)[/code]
      *
      * @return <code>true</code> if this converter is linear;
@@ -77,15 +75,14 @@ public abstract class UnitConverter implements Serializable {
      * converter specified. To be considered equal this converter
      * concatenated with the one specified must returns the {@link #IDENTITY}.
      *
-     * @param cvtr
-     *         the converter with which to compare.
+     * @param  cvtr the converter with which to compare.
      * @return <code>true</code> if the specified object is a converter
      *         considered equals to this converter;<code>false</code> otherwise.
      */
     public boolean equals(Object cvtr) {
-        if (this == cvtr) { return true; }
-        if (!(cvtr instanceof UnitConverter)) { return false;}
-        return this.concatenate(((UnitConverter) cvtr).inverse()) == IDENTITY;
+        if (this == cvtr) return true;
+        if (!(cvtr instanceof UnitConverter)) return false;
+        return this.concatenate(((UnitConverter)cvtr).inverse()) == IDENTITY;
     }
 
     /**
@@ -93,10 +90,10 @@ public abstract class UnitConverter implements Serializable {
      * hash codes.
      *
      * @return this converter hash code value.
-     * @see #equals
+     * @see    #equals
      */
     public int hashCode() {
-        return Float.floatToIntBits((float) convert(1.0));
+        return Float.floatToIntBits((float)convert(1.0));
     }
 
     /**
@@ -105,11 +102,10 @@ public abstract class UnitConverter implements Serializable {
      * and then converting by this converter.
      *
      * <p>Note: Implementations must ensure that the {@link #IDENTITY} instance
-     * is returned if the resulting converter is an identity
-     * converter.</p>
+     *          is returned if the resulting converter is an identity
+     *          converter.</p>
      *
-     * @param converter
-     *         the other converter.
+     * @param  converter the other converter.
      * @return the concatenation of this converter with the other converter.
      */
     public UnitConverter concatenate(UnitConverter converter) {
@@ -164,10 +160,8 @@ public abstract class UnitConverter implements Serializable {
          * Creates a compound converter resulting from the combined
          * transformation of the specified converters.
          *
-         * @param first
-         *         the first converter.
-         * @param second
-         *         the second converter.
+         * @param  first the first converter.
+         * @param  second the second converter.
          */
         private Compound(UnitConverter first, UnitConverter second) {
             _first = first;
